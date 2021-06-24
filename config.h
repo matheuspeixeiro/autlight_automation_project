00825e41da6a63abc528d5c8f57d82660312a267
matheuspeixeiro <65266315+matheuspeixeiro@users.noreply.github.com>
2021-06-24 10:54:10 -0300

    Added the arduino skecth
    Attachment: config.h -> Adafruit IO server instantiation library

---
+++ config.h
@@ -0,0 +1,11 @@
+// Dados MQTT
+#define   IO_USERNAME   "teuz007"
+#define   IO_KEY        "aio_oOby619aBZDiFZmFTPuTZ4OG8hND"
+
+// Dados Wi-Fi
+#define   WIFI_SSID   "peixeiroinformatica"
+#define   WIFI_PASS   "Ab034033"
+
+// Dados para comunicação
+#include "AdafruitIO_WiFi.h"
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
